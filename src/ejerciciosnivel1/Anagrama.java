1d8fdcfbaa97beecac42f1a52d6b197f4147828c
angel <angel@angel-pc>
2020-01-15 21:12:14 +0100

    15.1.20 Comienzo

---
+++ src/ejerciciosnivel1/Anagrama.java
@@ -0,0 +1,22 @@
+
+package ejerciciosnivel1;
+
+/**
+ *
+ * @author Angel Esquinas
+ */
+public class Anagrama {
+    
+    public static void main(String[] args) {
+        
+        Anagrama ejercicios=new Anagrama();
+        
+        ejercicios.anagrama("Alta Velocidad Española");
+    }
+    
+    public void anagrama(String palabra){
+        
+        
+        
+    }
+}
